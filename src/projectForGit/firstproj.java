e8d42416d1956f3b03826baf2374b1795f5876c4
shahy96 <shahidkabeer25@gmail.com>
2022-01-23 16:22:22 +0530

    largest values

--- src/projectForGit/firstproj.java
+++ src/projectForGit/firstproj.java
@@ -7,7 +7,32 @@ public class firstproj {
 		System.out.print("helloooo1");
 		for(int i=0; i < 10; i++){
 			System.out.println(i + " changes added");
-		} 	
+		}
+		int[] array = new int[7];
+		array[0] = 2;
+		array[1] = 4;
+		array[2] = 10;
+		array[3] = 12;
+		array[4] = 5;
+		array[5] = 7;
+		array[6] = 3;
+		
+		int small =array[0];
+		for(int  i = 0;i < array.length; i++) {
+			if(array[i] < small) {
+				small = array[i];
+			}		
+		}
+		System.out.print("smallest: " + small);
+		int large = array[0]; 
+		for(int i = 0;i < array.length; i++) {
+			if(array[i] > large) {
+				large = array[i];
+			}	
+			
+		}
+		System.out.println("Largest : "+ large);
+		
 
 		
 	}
