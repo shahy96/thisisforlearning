44872f6ca8e13a0d278c75ae27846de7ff34d7ce
shahy96 <shahidkabeer25@gmail.com>
2022-01-23 16:14:04 +0530

    commiting

--- src/projectForGit/firstproj.java
+++ src/projectForGit/firstproj.java
@@ -5,13 +5,10 @@ public class firstproj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("helloooo1");
-<<<<<<< HEAD
-
-=======
 		for(int i=0; i < 10; i++){
 			System.out.println(i);
 		} 	
->>>>>>> 9e24b743af79dc383c4ef4a9e8b914d1cc6dd874
+
 		
 	}
 
